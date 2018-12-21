b798ae558411ab7fb6d167210f767bdc011a492b
kor-al <korinalice@gmail.com>
2018-12-22 02:07:06 +0300

    Update dist_diagram.js

--- code/dist_diagram.js
+++ code/dist_diagram.js
@@ -1,19 +1,20 @@
 const Sizes = ['Small', 'Medium', 'Large']
-const Choices = ['All', 'Race', 'WalkSpeed', 'Level' , 'Size', 'Def', 'logHP', 'PropertyName',
-"PropertyLevel","AtkDelay" ],
-defaultChecked = 'All';
+const Choices = ['All', 'Race', 'WalkSpeed', 'Level', 'Size', 'Def', 'HP', 'PropertyName',
+    "PropertyLevel", "AtkDelay"
+  ],
+  defaultChecked = 'All';
 
 var xCenter = 0;
 var containerH = 0,
-containerW = 0;
+  containerW = 0;
 // const minAngle, maxAngle =
 var rScale = d3.scalePoint().range([3, 10]).domain(Sizes)
 const discrete_features = d3.set(["Size", "Race", "WalkSpeed", "DelayAfterHit", "PropertyName",
   "PropertyLevel"
 ])
-const quant_features = d3.set(["Level", "Def", "AtkDelay", "logHP"]);
+const quant_features = d3.set(["Level", "Def", "AtkDelay", "HP"]);
 const circleRadius = 150,
-      xPadding = 50;
+  circleTextPadding = 25;
 
 
 
@@ -34,15 +35,17 @@ function createDistDiagram(targetSVG, data) {
   containerH = height;
   containerW = width;
 
+  var grid = diagram.append("g").attr('id', 'grid')
+
   const node = diagram.append("g")
-    .selectAll("circle")
+    .selectAll(".node")
     .data(data)
     .enter().append("circle")
     .attr('r', function(d) {
       return rScale(d.Size)
     })
     .attr("class", function(d) {
-      return d.viz_group
+      return 'node ' + d.viz_group
     })
 
 }
@@ -50,21 +53,30 @@ function createDistDiagram(targetSVG, data) {
 function updateDistDiagram(data, choice) {
 
 
+
   var graph = d3.select('#distDia');
+  var grid = d3.select('#grid');
+  var node = graph.selectAll('.node');
+  //d3.selectAll('.group_label').transition().duration(200).attr('fill-opacity', 0).remove()
 
   var simulation = d3.forceSimulation(data)
-    .force('charge', d3.forceManyBody().strength(10))
+    .force('charge', d3.forceManyBody().strength(0))
     .force('center', d3.forceCenter(0, 0));
 
 
+  var isDiscrete = false;
+  var centerScale;
+
+  d3.selectAll('.group_label').remove()
   if (choice == 'All') {
     simulation.force('x', d3.forceX().x(xCenter));
   } else {
-    console.log(choice)
     if (discrete_features.has(choice)) {
       var unique = d3.set(data.map(function(d) {
         return d[choice];
       })).values()
+
+      isDiscrete = true;
 
       var angleScale = d3.scalePoint().padding(0.5).range([0, 2 * Math.PI]).domain(unique)
 
@@ -80,109 +92,202 @@ function updateDistDiagram(data, choice) {
         }))
     } else {
 
-      // var unique = d3.set(data.map(function(d) {
-      //   return d[choice];
-      // })).values()
-      // var centerScale = d3.scalePoint().padding(1).range([xLimLeft, xLimRight]).domain(unique)
-      console.log(d3.extent(data, function(d) {
+      var unique = d3.set(data.map(function(d) {
         return +d[choice];
-      }))
+      })).values().sort(function(a, b) {
+        return +a < +b ? -1 : +a > +b ? 1 : 0;
+      }) //}d3.ascending)
 
 
-      var centerScale = d3.scaleLinear()
-        .range([-containerW/2, containerW/2])
-        .domain(d3.extent(data, function(d) {
-          return +d[choice];
-        }))
+
+      centerScale = d3.scalePoint().padding(1).range([-containerW / 2, containerW / 2]).domain(unique)
+
+      // var centerScale = d3.scaleLinear()
+      //   .range([-containerW/2, containerW/2])
+      //   .domain(d3.extent(data, function(d) {
+      //     return +d[choice];
+      //   }))
 
       simulation.force('x', d3.forceX().x(function(d) {
         return centerScale(+d[choice]);
       }))
+
+      //AXIS
+      var ticks = unique.filter(function(d, i) {
+        return !(i % 5);
+      });
+      console.log(unique)
+      console.log(ticks)
+      var rects = grid.append('g').classed('group_label', true)
+      var ticks_text = grid.append('g').classed('group_label', true)
+      ticks.forEach(function(t, i) {
+        // labels.append('line')
+        // .attr("stroke", "#cccccc").attr('y2', -containerH/2).attr('x2', centerScale(+t))
+        // .attr('y1', containerH/2).attr('x1', centerScale(+t));
+        rects.append('rect').attr('fill', "#cccccc").attr('fill-opacity', 1/ticks.length)
+          .attr('y', -containerH / 2).attr('x', -containerW / 2).attr('height', containerH)
+          .attr('width', containerW / 2 + centerScale(+t))
+        console.log(centerScale(+t))
+        ticks_text.append('text').attr('y', containerH / 2 - 20).attr('x', centerScale(+t))
+        .text(format_value(Math.ceil(t)))
+      })
     }
   }
 
-
-  var node = graph.selectAll('circle');
 
   simulation.force('collision', d3.forceCollide().radius(function(d) {
       return rScale(d.Size) + 1;
     }))
-    .on('tick', ticked);
+    .on('tick', ticked)
+    .on('end', show_grid);
 
 
   function ticked() {
-
+    // without boundaries
     // node.attr("cx", d => d.x)
     //   .attr("cy", d => d.y);
 
+    //now WITH boundaries
     node.attr("cx", function(d) {
-       var radius = rScale(d.Size)
-       d.x = containerW/2 + d.x
-       return d.x = Math.max(radius, Math.min(containerW - radius, d.x)) - containerW/2;
-     })
-       .attr("cy", function(d) {
-         var radius = rScale(d.Size)
-         d.y = containerW/2 + d.y
-          return d.y = Math.max(radius, Math.min(containerH - radius, d.y)) - containerH/2;
-        });
-
-
+        var radius = rScale(d.Size)
+        d.x = containerW / 2 + d.x
+        return d.x = Math.max(radius, Math.min(containerW - radius, d.x)) - containerW / 2;
+      })
+      .attr("cy", function(d) {
+        var radius = rScale(d.Size)
+        d.y = containerW / 2 + d.y
+        return d.y = Math.max(radius, Math.min(containerH - radius, d.y)) - containerH / 2;
+      });
   }
 
-}
+  function show_grid() {
+    var labels = graph.append('g').attr('id', 'group_labels');
 
-function create_buttons(){
+    if (isDiscrete) {
 
-// Create the shape selectors
-var form = d3.select("body").append("form");
+      var positions = []
+      node.each(function(d, i) {
+        //console.log(d,i) //data_enter
+        var thisCircle = d3.select(this)
+        positions.push({
+          cx: +thisCircle.attr('cx'),
+          cy: +thisCircle.attr('cy'),
+          value: d[choice]
+        })
+      })
+      console.log(positions)
+      console.log(node)
 
-labels = form.selectAll("label")
-    .data(Choices)
-    .enter()
-    .append("label")
-    .text(function(d) {return d == 'All'? 'Show ' + d : 'By ' + d;})
-    .insert("input")
-    .attr('type', 'radio')
-    .attr('class', 'radioChoice')
-    .attr('name', 'sorting_mode')
-    .attr('value', function(d, i) {return d;})
-    .property("checked", function(d, i) {return i===defaultChecked ;});
-}
+      var nest_pos = d3.nest()
+        .key(function(d) {
+          return d.value;
+        })
+        .rollup(function(d) {
+          return {
+            xRange: d3.extent(d, function(g) {
+              return +g.cx;
+            }),
+            yRange: d3.extent(d, function(g) {
+              return +g.cy;
+            })
+          };
+        }).entries(positions);
+      //
+      // //console.log(nest_pos)
+      //
+      nest_pos.forEach(function(p, i) {
+        console.log(p, i)
+        var groupR = Math.max(p.value.xRange[1] - p.value.xRange[0], p.value.yRange[1] - p.value.yRange[0]) / 2
+        var groupC = {
+          cx: (p.value.xRange[1] + p.value.xRange[0]) / 2,
+          cy: (p.value.yRange[1] + p.value.yRange[0]) / 2
+        }
+        // console.log(groupR, groupC)
+        // graph.append('circle').attr('r', groupR).attr('cx', groupC.cx).attr('cy', groupC.cy)
+        var label_angle = -Math.PI / 3; //(groupC.cx > 0) ? -Math.PI/3 : Math.PI/3;
+        var anchor = polarToCartesian(label_angle, groupR + circleTextPadding)
+        labels.append('text')
+          .attr('x', groupC.cx + anchor.x)
+          .attr('y', groupC.cy + anchor.y)
+          .text(unique[i])
+          .attr('fill-opacity', 0)
+          .classed('group_label', true)
+          .transition().duration(500).attr('fill-opacity', 1)
+      })
 
-function handle_buttons(data) {
-  const buttons = d3.selectAll('input');
-  updateDistDiagram(data, 'All');
 
-  buttons.on('change', function(d) {
-    console.log('button changed to ' + this.value);
-    updateDistDiagram(data, this.value);
-  });
-}
-
-function reset_buttons(){
-  var inp = document.getElementsByTagName('input')
-  for (var i = inp.length-1; i>=0; i--) {
-    if ('radio'===inp[i].type) {
-      if (inp[i].value == 'All') inp[i].checked = true;
-      else inp[i].checked = false;
     }
   }
 }
 
 
-function getScale(data, choice) {
-  if (discrete_features.has(feature)) {
-    var unique = d3.set(data.map(function(d) {
-      return d[choice];
-    })).values()
-    var numDifVals = unique.length;
-    var step = 2 * Math.PI / numDifVals;
-    var anchors = []
-    for (let i = 0; i < numDifVals; i++) {
-      anchors.push(i * step);
+  // if (isDiscrete)
+  // {
+  //   for(let i = 0; i< unique.length; i++){
+  //         var anchor =  polarToCartesian(angleScale(unique[i]), circleRadius + circleTextPadding);
+  //         labels.append('text').attr('x', anchor.x).attr('y', anchor.y)
+  //         .text(unique[i])
+  //       }
+  // }
+
+  function create_buttons() {
+
+    // Create the shape selectors
+    var form = d3.select("body").append("form");
+
+    labels = form.selectAll("label")
+      .data(Choices)
+      .enter()
+      .append("label")
+      .text(function(d) {
+        return d == 'All' ? 'Show ' + d : 'By ' + d;
+      })
+      .insert("input")
+      .attr('type', 'radio')
+      .attr('class', 'radioChoice')
+      .attr('name', 'sorting_mode')
+      .attr('value', function(d, i) {
+        return d;
+      })
+      .property("checked", function(d, i) {
+        return i === defaultChecked;
+      });
+  }
+
+  function handle_buttons(data) {
+    const buttons = d3.selectAll('input');
+    updateDistDiagram(data, 'All');
+
+    buttons.on('change', function(d) {
+      console.log('button changed to ' + this.value);
+      updateDistDiagram(data, this.value);
+    });
+  }
+
+  function reset_buttons() {
+    var inp = document.getElementsByTagName('input')
+    for (var i = inp.length - 1; i >= 0; i--) {
+      if ('radio' === inp[i].type) {
+        if (inp[i].value == 'All') inp[i].checked = true;
+        else inp[i].checked = false;
+      }
     }
   }
-  return d3.scalePoint().padding(1).range([xLimLeft, xLimRight]).domain(unique)
 
 
-}
+  // function show_grid(data, choice) {
+  //   if (discrete_features.has(feature)) {
+  //     var unique = d3.set(data.map(function(d) {
+  //       return d[choice];
+  //     })).values()
+  //     var numDifVals = unique.length;
+  //     var step = 2 * Math.PI / numDifVals;
+  //     var anchors = []
+  //     for (let i = 0; i < numDifVals; i++) {
+  //       anchors.push(i * step);
+  //     }
+  //   }
+  //   return d3.scalePoint().padding(1).range([xLimLeft, xLimRight]).domain(unique)
+  //
+  //
+  // }
